d86248f0047ee674a25edf0705c3610a45595549
Mackenzie Baack <mackenzi.2399315@nv.ccsd.net>
2020-09-23 17:20:08 +0000

    Worked on ConstructionTester, most of what we did in class, I went a little bit ahead.

--- src/main/java/ConstructionTester.java
+++ src/main/java/ConstructionTester.java
@@ -4,6 +4,20 @@ public class ConstructionTester
 {
     public static void main(String[] args)
     {
-        // Start here!
+        Scanner in = new Scanner(System.in);
+        double numBoards, numWindow, taxRate, total;
+
+        System.out.println("Enter the sales tax rate:");
+        taxRate = in.nextDouble();
+        System.out.println("How many boards do you need?");
+        numBoards = in.nextDouble();
+        System.out.println("How many windows do you need?");
+        numWindow = in.nextDouble();
+
+        Construction construct = new Construction(8.0, 11.0, taxRate);
+
+        total = construct.lumberCost(numBoards) + construct.windowCost(numWindows);
+
+        
     }
 }
