16c1d7e59b9525860d13749e2f7d996bf0463b2e
Mackenzie Baack <mackenzi.2399315@nv.ccsd.net>
2020-09-23 17:24:00 +0000

    Finished Construction Costs

--- src/main/java/ConstructionTester.java
+++ src/main/java/ConstructionTester.java
@@ -5,19 +5,22 @@ public class ConstructionTester
     public static void main(String[] args)
     {
         Scanner in = new Scanner(System.in);
-        double numBoards, numWindow, taxRate, total;
+        double taxRate, total;
+        int numBoards, numWindows;
 
         System.out.println("Enter the sales tax rate:");
         taxRate = in.nextDouble();
         System.out.println("How many boards do you need?");
-        numBoards = in.nextDouble();
+        numBoards = in.nextInt();
         System.out.println("How many windows do you need?");
-        numWindow = in.nextDouble();
+        numWindows = in.nextInt();
 
         Construction construct = new Construction(8.0, 11.0, taxRate);
 
         total = construct.lumberCost(numBoards) + construct.windowCost(numWindows);
 
-        
+        System.out.println("Total: " + total);
+        System.out.println("Grand Total: " + construct.grandTotal(total));
+
     }
 }
